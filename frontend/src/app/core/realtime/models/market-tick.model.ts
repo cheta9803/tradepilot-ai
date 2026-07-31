51cce60cc88511eb2b0efaf05450e9c1e87c52c4
Chetankumar Patel <chetankumarpatel@Chetankumars-MacBook-Air.local>
2026-07-31 23:16:36 +0530

    feat(dashboard): complete dashboard backend

---
+++ frontend/src/app/core/realtime/models/market-tick.model.ts
@@ -0,0 +1,17 @@
+export interface MarketTick {
+  exchange: string;
+  symbol: string;
+  trading_symbol: string;
+  token: string;
+
+  ltp: number;
+
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+
+  volume: number;
+
+  timestamp: string;
+}
